a8d5cc213445ce723f93ebba6c89b944befcefc0
Helcio <helcioanicio19@gmail.com>
2024-04-09 22:50:14 -0300

    Chore: Realized some tests with routs to my API

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify';
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'Hello NLW unite'
+})
+
+app.get('/test', () => {
+  return 'Hello Test'
+})
+
+app.listen({port: 3333}).then(() => {
+  console.log('HTTP server runing')
+})
